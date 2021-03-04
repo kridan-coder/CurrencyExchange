4d46f074a95663ed68232476da40db00fff10304
kridan-coder <krivelevdaniil@gmail.com>
2021-03-04 17:42:32 +0700

    ui done

--- CurRate/Form1.Designer.cs
+++ CurRate/Form1.Designer.cs
@@ -31,23 +31,34 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.nextDayBtn = new System.Windows.Forms.Button();
             this.edRate = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.edDays = new System.Windows.Forms.NumericUpDown();
-            this.btCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.setStartPriceBtn = new System.Windows.Forms.Button();
+            this.startGameBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sell1Dollar = new System.Windows.Forms.Button();
+            this.buy1Dollar = new System.Windows.Forms.Button();
+            this.avalRlbl = new System.Windows.Forms.Label();
+            this.avalDlbl = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.edRate)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.edDays)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.btCalculate);
-            this.panel1.Controls.Add(this.edDays);
+            this.panel1.Controls.Add(this.avalDlbl);
+            this.panel1.Controls.Add(this.avalRlbl);
+            this.panel1.Controls.Add(this.buy1Dollar);
+            this.panel1.Controls.Add(this.sell1Dollar);
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.startGameBtn);
+            this.panel1.Controls.Add(this.setStartPriceBtn);
+            this.panel1.Controls.Add(this.nextDayBtn);
             this.panel1.Controls.Add(this.edRate);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -56,58 +67,38 @@
             this.panel1.Size = new System.Drawing.Size(758, 100);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // nextDayBtn
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Текущий курс";
+            this.nextDayBtn.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nextDayBtn.Location = new System.Drawing.Point(278, 34);
+            this.nextDayBtn.Name = "nextDayBtn";
+            this.nextDayBtn.Size = new System.Drawing.Size(111, 37);
+            this.nextDayBtn.TabIndex = 4;
+            this.nextDayBtn.Text = "Next day!";
+            this.nextDayBtn.UseVisualStyleBackColor = true;
+            this.nextDayBtn.Click += new System.EventHandler(this.btCalculate_Click);
             // 
             // edRate
             // 
             this.edRate.DecimalPlaces = 2;
-            this.edRate.Location = new System.Drawing.Point(127, 18);
+            this.edRate.Location = new System.Drawing.Point(85, 19);
             this.edRate.Name = "edRate";
             this.edRate.Size = new System.Drawing.Size(97, 20);
             this.edRate.TabIndex = 1;
             this.edRate.Value = new decimal(new int[] {
-            8953,
-            0,
-            0,
-            131072});
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 60);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Дней";
-            // 
-            // edDays
-            // 
-            this.edDays.Location = new System.Drawing.Point(127, 53);
-            this.edDays.Name = "edDays";
-            this.edDays.Size = new System.Drawing.Size(97, 20);
-            this.edDays.TabIndex = 3;
-            this.edDays.Value = new decimal(new int[] {
-            7,
+            5,
             0,
             0,
             0});
             // 
-            // btCalculate
+            // label1
             // 
-            this.btCalculate.Location = new System.Drawing.Point(309, 36);
-            this.btCalculate.Name = "btCalculate";
-            this.btCalculate.Size = new System.Drawing.Size(75, 23);
-            this.btCalculate.TabIndex = 4;
-            this.btCalculate.Text = "Рассчитать";
-            this.btCalculate.UseVisualStyleBackColor = true;
-            this.btCalculate.Click += new System.EventHandler(this.btCalculate_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start $ price:";
             // 
             // chart1
             // 
@@ -129,6 +120,86 @@
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
             // 
+            // setStartPriceBtn
+            // 
+            this.setStartPriceBtn.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.setStartPriceBtn.Location = new System.Drawing.Point(16, 47);
+            this.setStartPriceBtn.Name = "setStartPriceBtn";
+            this.setStartPriceBtn.Size = new System.Drawing.Size(166, 37);
+            this.setStartPriceBtn.TabIndex = 5;
+            this.setStartPriceBtn.Text = "SET";
+            this.setStartPriceBtn.UseVisualStyleBackColor = true;
+            // 
+            // startGameBtn
+            // 
+            this.startGameBtn.Font = new System.Drawing.Font("Montserrat", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startGameBtn.Location = new System.Drawing.Point(493, 6);
+            this.startGameBtn.Name = "startGameBtn";
+            this.startGameBtn.Size = new System.Drawing.Size(258, 88);
+            this.startGameBtn.TabIndex = 6;
+            this.startGameBtn.Text = "START GAME!";
+            this.startGameBtn.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Available ₽:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Available $:";
+            // 
+            // sell1Dollar
+            // 
+            this.sell1Dollar.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sell1Dollar.Location = new System.Drawing.Point(395, 34);
+            this.sell1Dollar.Name = "sell1Dollar";
+            this.sell1Dollar.Size = new System.Drawing.Size(72, 36);
+            this.sell1Dollar.TabIndex = 9;
+            this.sell1Dollar.Text = "Sell 1$";
+            this.sell1Dollar.UseVisualStyleBackColor = true;
+            // 
+            // buy1Dollar
+            // 
+            this.buy1Dollar.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buy1Dollar.Location = new System.Drawing.Point(200, 34);
+            this.buy1Dollar.Name = "buy1Dollar";
+            this.buy1Dollar.Size = new System.Drawing.Size(72, 36);
+            this.buy1Dollar.TabIndex = 10;
+            this.buy1Dollar.Text = "Buy 1$";
+            this.buy1Dollar.UseVisualStyleBackColor = true;
+            // 
+            // avalRlbl
+            // 
+            this.avalRlbl.AutoSize = true;
+            this.avalRlbl.Location = new System.Drawing.Point(359, 12);
+            this.avalRlbl.Name = "avalRlbl";
+            this.avalRlbl.Size = new System.Drawing.Size(25, 13);
+            this.avalRlbl.TabIndex = 11;
+            this.avalRlbl.Text = "500";
+            this.avalRlbl.UseMnemonic = false;
+            this.avalRlbl.UseWaitCursor = true;
+            // 
+            // avalDlbl
+            // 
+            this.avalDlbl.AutoSize = true;
+            this.avalDlbl.Location = new System.Drawing.Point(359, 78);
+            this.avalDlbl.Name = "avalDlbl";
+            this.avalDlbl.Size = new System.Drawing.Size(13, 13);
+            this.avalDlbl.TabIndex = 12;
+            this.avalDlbl.Text = "0";
+            this.avalDlbl.UseMnemonic = false;
+            this.avalDlbl.UseWaitCursor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -141,7 +212,6 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.edRate)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.edDays)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
@@ -150,12 +220,18 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btCalculate;
-        private System.Windows.Forms.NumericUpDown edDays;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button nextDayBtn;
         private System.Windows.Forms.NumericUpDown edRate;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button startGameBtn;
+        private System.Windows.Forms.Button setStartPriceBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buy1Dollar;
+        private System.Windows.Forms.Button sell1Dollar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label avalDlbl;
+        private System.Windows.Forms.Label avalRlbl;
     }
 }
 
