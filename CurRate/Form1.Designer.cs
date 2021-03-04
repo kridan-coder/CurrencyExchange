db6d52289f998ba82807053bff13b3d1b2b30e29
kridan-coder <krivelevdaniil@gmail.com>
2021-03-04 18:14:47 +0700

    lab4 done

--- CurRate/Form1.Designer.cs
+++ CurRate/Form1.Designer.cs
@@ -28,21 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.avalDlbl = new System.Windows.Forms.Label();
+            this.avalRlbl = new System.Windows.Forms.Label();
+            this.buy1Dollar = new System.Windows.Forms.Button();
+            this.sell1Dollar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startGameBtn = new System.Windows.Forms.Button();
+            this.setStartPriceBtn = new System.Windows.Forms.Button();
             this.nextDayBtn = new System.Windows.Forms.Button();
             this.edRate = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.setStartPriceBtn = new System.Windows.Forms.Button();
-            this.startGameBtn = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.sell1Dollar = new System.Windows.Forms.Button();
-            this.buy1Dollar = new System.Windows.Forms.Button();
-            this.avalRlbl = new System.Windows.Forms.Label();
-            this.avalDlbl = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.edRate)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
@@ -67,6 +67,90 @@
             this.panel1.Size = new System.Drawing.Size(758, 100);
             this.panel1.TabIndex = 0;
             // 
+            // avalDlbl
+            // 
+            this.avalDlbl.AutoSize = true;
+            this.avalDlbl.Location = new System.Drawing.Point(359, 78);
+            this.avalDlbl.Name = "avalDlbl";
+            this.avalDlbl.Size = new System.Drawing.Size(13, 13);
+            this.avalDlbl.TabIndex = 12;
+            this.avalDlbl.Text = "0";
+            this.avalDlbl.UseMnemonic = false;
+            this.avalDlbl.UseWaitCursor = true;
+            // 
+            // avalRlbl
+            // 
+            this.avalRlbl.AutoSize = true;
+            this.avalRlbl.Location = new System.Drawing.Point(359, 12);
+            this.avalRlbl.Name = "avalRlbl";
+            this.avalRlbl.Size = new System.Drawing.Size(25, 13);
+            this.avalRlbl.TabIndex = 11;
+            this.avalRlbl.Text = "500";
+            this.avalRlbl.UseMnemonic = false;
+            this.avalRlbl.UseWaitCursor = true;
+            // 
+            // buy1Dollar
+            // 
+            this.buy1Dollar.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buy1Dollar.Location = new System.Drawing.Point(200, 34);
+            this.buy1Dollar.Name = "buy1Dollar";
+            this.buy1Dollar.Size = new System.Drawing.Size(72, 36);
+            this.buy1Dollar.TabIndex = 10;
+            this.buy1Dollar.Text = "Buy 1$";
+            this.buy1Dollar.UseVisualStyleBackColor = true;
+            this.buy1Dollar.Click += new System.EventHandler(this.buy1Dollar_Click);
+            // 
+            // sell1Dollar
+            // 
+            this.sell1Dollar.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sell1Dollar.Location = new System.Drawing.Point(395, 34);
+            this.sell1Dollar.Name = "sell1Dollar";
+            this.sell1Dollar.Size = new System.Drawing.Size(72, 36);
+            this.sell1Dollar.TabIndex = 9;
+            this.sell1Dollar.Text = "Sell 1$";
+            this.sell1Dollar.UseVisualStyleBackColor = true;
+            this.sell1Dollar.Click += new System.EventHandler(this.sell1Dollar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Available $:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Available ₽:";
+            // 
+            // startGameBtn
+            // 
+            this.startGameBtn.Font = new System.Drawing.Font("Montserrat", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startGameBtn.Location = new System.Drawing.Point(493, 6);
+            this.startGameBtn.Name = "startGameBtn";
+            this.startGameBtn.Size = new System.Drawing.Size(258, 88);
+            this.startGameBtn.TabIndex = 6;
+            this.startGameBtn.Text = "START GAME!";
+            this.startGameBtn.UseVisualStyleBackColor = true;
+            this.startGameBtn.Click += new System.EventHandler(this.startGameBtn_Click);
+            // 
+            // setStartPriceBtn
+            // 
+            this.setStartPriceBtn.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.setStartPriceBtn.Location = new System.Drawing.Point(16, 47);
+            this.setStartPriceBtn.Name = "setStartPriceBtn";
+            this.setStartPriceBtn.Size = new System.Drawing.Size(166, 37);
+            this.setStartPriceBtn.TabIndex = 5;
+            this.setStartPriceBtn.Text = "SET";
+            this.setStartPriceBtn.UseVisualStyleBackColor = true;
+            this.setStartPriceBtn.Click += new System.EventHandler(this.setStartPriceBtn_Click);
+            // 
             // nextDayBtn
             // 
             this.nextDayBtn.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -76,17 +160,22 @@
             this.nextDayBtn.TabIndex = 4;
             this.nextDayBtn.Text = "Next day!";
             this.nextDayBtn.UseVisualStyleBackColor = true;
-            this.nextDayBtn.Click += new System.EventHandler(this.btCalculate_Click);
+            this.nextDayBtn.Click += new System.EventHandler(this.nextDayBtn_Click);
             // 
             // edRate
             // 
             this.edRate.DecimalPlaces = 2;
             this.edRate.Location = new System.Drawing.Point(85, 19);
+            this.edRate.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             this.edRate.Name = "edRate";
             this.edRate.Size = new System.Drawing.Size(97, 20);
             this.edRate.TabIndex = 1;
             this.edRate.Value = new decimal(new int[] {
-            5,
+            70,
             0,
             0,
             0});
@@ -102,103 +191,23 @@
             // 
             // chart1
             // 
-            chartArea1.AxisX.Minimum = 0D;
-            chartArea1.AxisY.Minimum = 50D;
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Minimum = 50D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.chart1.Location = new System.Drawing.Point(0, 100);
             this.chart1.Name = "chart1";
-            series1.BorderWidth = 3;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.IsValueShownAsLabel = true;
-            series1.LabelFormat = "f2";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsValueShownAsLabel = true;
+            series2.LabelFormat = "f2";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(758, 350);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
-            // 
-            // setStartPriceBtn
-            // 
-            this.setStartPriceBtn.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.setStartPriceBtn.Location = new System.Drawing.Point(16, 47);
-            this.setStartPriceBtn.Name = "setStartPriceBtn";
-            this.setStartPriceBtn.Size = new System.Drawing.Size(166, 37);
-            this.setStartPriceBtn.TabIndex = 5;
-            this.setStartPriceBtn.Text = "SET";
-            this.setStartPriceBtn.UseVisualStyleBackColor = true;
-            // 
-            // startGameBtn
-            // 
-            this.startGameBtn.Font = new System.Drawing.Font("Montserrat", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.startGameBtn.Location = new System.Drawing.Point(493, 6);
-            this.startGameBtn.Name = "startGameBtn";
-            this.startGameBtn.Size = new System.Drawing.Size(258, 88);
-            this.startGameBtn.TabIndex = 6;
-            this.startGameBtn.Text = "START GAME!";
-            this.startGameBtn.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(289, 12);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Available ₽:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(289, 78);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Available $:";
-            // 
-            // sell1Dollar
-            // 
-            this.sell1Dollar.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.sell1Dollar.Location = new System.Drawing.Point(395, 34);
-            this.sell1Dollar.Name = "sell1Dollar";
-            this.sell1Dollar.Size = new System.Drawing.Size(72, 36);
-            this.sell1Dollar.TabIndex = 9;
-            this.sell1Dollar.Text = "Sell 1$";
-            this.sell1Dollar.UseVisualStyleBackColor = true;
-            // 
-            // buy1Dollar
-            // 
-            this.buy1Dollar.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buy1Dollar.Location = new System.Drawing.Point(200, 34);
-            this.buy1Dollar.Name = "buy1Dollar";
-            this.buy1Dollar.Size = new System.Drawing.Size(72, 36);
-            this.buy1Dollar.TabIndex = 10;
-            this.buy1Dollar.Text = "Buy 1$";
-            this.buy1Dollar.UseVisualStyleBackColor = true;
-            // 
-            // avalRlbl
-            // 
-            this.avalRlbl.AutoSize = true;
-            this.avalRlbl.Location = new System.Drawing.Point(359, 12);
-            this.avalRlbl.Name = "avalRlbl";
-            this.avalRlbl.Size = new System.Drawing.Size(25, 13);
-            this.avalRlbl.TabIndex = 11;
-            this.avalRlbl.Text = "500";
-            this.avalRlbl.UseMnemonic = false;
-            this.avalRlbl.UseWaitCursor = true;
-            // 
-            // avalDlbl
-            // 
-            this.avalDlbl.AutoSize = true;
-            this.avalDlbl.Location = new System.Drawing.Point(359, 78);
-            this.avalDlbl.Name = "avalDlbl";
-            this.avalDlbl.Size = new System.Drawing.Size(13, 13);
-            this.avalDlbl.TabIndex = 12;
-            this.avalDlbl.Text = "0";
-            this.avalDlbl.UseMnemonic = false;
-            this.avalDlbl.UseWaitCursor = true;
             // 
             // Form1
             // 
--- CurRate/Form1.cs
+++ CurRate/Form1.cs
@@ -17,21 +17,94 @@ namespace CurRate
             InitializeComponent();
         }
 
+        bool gameIsRunning = false;
 
-        const double k = 0.02;
+        const double k = 0.05;
 
-        private void btCalculate_Click(object sender, EventArgs e)
+        int daysSpent = 0;
+        
+        double startRate = 70.00;
+        double roubles = 500;
+        int dollars = 0;
+
+        double rate;
+
+        Random random = new Random();
+
+        private void refreshUI()
         {
-            Random random = new Random();
-            double rate = (double)edRate.Value;
-            int days = (int)edDays.Value;
+            avalDlbl.Text = dollars.ToString();
+            avalRlbl.Text = roubles.ToString();
+        }
+
+        private void restartGame()
+        {
+
+            gameIsRunning = true;
+
+            rate = startRate;
+
+            roubles = 500;
+            dollars = 0;
+            daysSpent = 0;
+
+            startGameBtn.Text = "RESTART GAME!";
+            refreshUI();
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = daysSpent + 1;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+
             chart1.Series[0].Points.Clear();
             chart1.Series[0].Points.AddXY(0, rate);
+        }
 
-            for (int day = 1; day <= days; day++)
+
+
+
+
+        private void nextDayBtn_Click(object sender, EventArgs e)
+        {
+            if (gameIsRunning)
             {
-                rate = rate * (1 + k * (random.NextDouble() - 0.5));
-                chart1.Series[0].Points.AddXY(day, rate);
+                if (daysSpent > 20)
+                {
+                    chart1.ChartAreas[0].AxisX.Minimum = daysSpent - 20;
+                }
+                rate = (rate * (1 + k * (random.NextDouble() - 0.5))) >= 0 ? rate * (1 + k * (random.NextDouble() - 0.5)) : 0.01;
+                chart1.ChartAreas[0].AxisX.Maximum = daysSpent + 1;
+                chart1.Series[0].Points.AddXY(++daysSpent, rate);
+                refreshUI();
+            }
+        }
+
+        private void startGameBtn_Click(object sender, EventArgs e)
+        {
+            restartGame();
+        }
+
+        private void setStartPriceBtn_Click(object sender, EventArgs e)
+        {
+            startRate = (double)edRate.Value;
+        }
+
+        private void buy1Dollar_Click(object sender, EventArgs e)
+        {
+            if (gameIsRunning && roubles - rate >= 0)
+            {
+                dollars++;
+                roubles -= rate;
+                refreshUI();
+            }
+        }
+
+        private void sell1Dollar_Click(object sender, EventArgs e)
+        {
+            if (gameIsRunning && dollars > 0)
+            {
+                dollars--;
+                roubles += rate;
+                refreshUI();
             }
         }
     }
